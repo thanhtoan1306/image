7dd0b8d0d4be014ff95664d4eadd3e4d4984524e
vu111293 <vu111293@mail.com>
2020-03-18 22:12:28 +0700

    fix error

--- lib/main.dart
+++ lib/main.dart
@@ -2,44 +2,54 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
 
-class _MyAppState extends State<MyApp> {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Main Screen'),
-        ),
-        body: Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Text(
-                  'Image',
-                  style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.lightBlue),
-                ),
-                FlatButton(
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (_) => new AlertDialog(
-                              title: new Text("Dialog Title"),
-                              content: new Text("This is my content"),
-                            ));
-                  },
-                  child: Text('Selected'),
-                ),
-              ],
-            ),
+      title: "App name here",
+      home: HomePage(),
+    );
+  }
+}
+
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main Screen'),
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text(
+                'Image',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.lightBlue),
+              ),
+              FlatButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (_) => new AlertDialog(
+                        title: new Text("Dialog Title"),
+                        content: new Text("This is my content"),
+                      ));
+                },
+                child: Text('Selected'),
+              ),
+            ],
           ),
         ),
       ),
