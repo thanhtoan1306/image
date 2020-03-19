9551d5745bece4943d237a2d0e2a50a95c3e6b07
letoan <letoan0608@gmail.com>
2020-03-19 17:08:56 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() => runApp(MyApp());
 
@@ -19,7 +22,70 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+
 class _HomePageState extends State<HomePage> {
+  File _image;
+
+  _openGallary(BuildContext context)async {
+    var image = await ImagePicker.pickImage(source: ImageSource.gallery);
+
+    this.setState(() {
+      _image = image;
+    });
+    Navigator.of(context).pop();
+
+
+  }
+  _openCamera(BuildContext context) async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    this.setState(() {
+      _image = image;
+    });
+    Navigator.of(context).pop();
+
+  }
+  Future<void> _showChoiceDiaLog(BuildContext context) {
+    return showDialog(context: context, builder: (BuildContext){
+      return AlertDialog(
+        title: Text('Gallary or Camera'),
+        content: SingleChildScrollView(
+          child: ListBody(
+
+            children: <Widget>[
+              GestureDetector(
+                child: Text('Gallary'),
+                onTap: () {
+                  _openGallary(context);
+                },
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+              ),
+              GestureDetector(
+                child: Text('Camera'),
+                onTap: () {
+                  _openCamera(context);
+                },
+              ),
+
+            ],
+          ),
+        ),
+      );
+
+
+    });
+  }
+  Widget _decideImageView(){
+    if(_image == null) {
+      return Image.asset('assets/placeholder.png');
+    }else{
+      return Image.file(_image,width: 400,height: 400,);
+      //print("anh da in");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,22 +97,14 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              Text(
-                'Image',
-                style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.lightBlue),
-              ),
+              Text('Image',style: TextStyle(fontWeight:FontWeight.bold,color:Colors.lightBlue),),
+
+              _decideImageView(),
               FlatButton(
                 onPressed: () {
-                  showDialog(
-                      context: context,
-                      builder: (_) => new AlertDialog(
-                        title: new Text("Dialog Title"),
-                        content: new Text("This is my content"),
-                      ));
+                  _showChoiceDiaLog(context);
                 },
+
                 child: Text('Selected'),
               ),
             ],
