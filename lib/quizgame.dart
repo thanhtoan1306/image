33896abf6e7207c5f943705ad1ee2aeef1b215b8
letoan <letoan0608@gmail.com>
2020-03-28 14:37:42 +0700

    update

--- lib/quizgame.dart
+++ lib/quizgame.dart
@@ -12,13 +12,54 @@ class MyQuizGame extends StatelessWidget {
   }
 }
 
+class ButtonItem extends StatefulWidget {
+  bool isCorrect;
+  Function checkCorrect;
+  ButtonItem({this.isCorrect, this.checkCorrect});
+  @override
+  _ButtonItemState createState() => _ButtonItemState();
+}
+
+class _ButtonItemState extends State<ButtonItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      alignment: Alignment.center,
+      child: Center(
+          child: InkWell(
+        child: Container(
+          width: 100,
+          height: 30,
+          decoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(15.0),
+            color: (widget.isCorrect ? Colors.green : Colors.red),
+          ),
+          child: Center(
+            child: Text(
+              'Check',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        onTap: widget.checkCorrect,
+      )),
+    );
+  }
+}
+
 class RectItem extends StatefulWidget {
   //hàm khởi tạo
   Function onClick;
   bool isActivate;
   String imgURL;
   int id;
-  RectItem({this.imgURL, this.id, this.onClick,this.isActivate}); //
+  RectItem({this.imgURL, this.id, this.onClick, this.isActivate}); //
   @override
   _RectItemState createState() => _RectItemState();
 }
@@ -30,14 +71,16 @@ class _RectItemState extends State<RectItem> {
       color: Colors.white,
       child: InkWell(
         child: Container(
-          width: 170,
-          height: 220,
+          width: 175,
+          height: 230,
           decoration: BoxDecoration(
             color: Colors.white,
             shape: BoxShape.rectangle,
             borderRadius: BorderRadius.circular(15.0),
-            boxShadow: const [BoxShadow(blurRadius: 5.0)],
-            border: Border.all(color: widget.isActivate?Colors.red: Colors.white, width: 1.0),
+            boxShadow: const [BoxShadow(blurRadius: 3.0)],
+            border: Border.all(
+                color: widget.isActivate ? Colors.blue : Colors.white,
+                width: 3.0),
           ),
           child: Image.asset(widget.imgURL),
         ),
@@ -52,20 +95,44 @@ class QuestionQuiz extends StatefulWidget {
   _QuestionQuizState createState() => _QuestionQuizState();
 }
 
-var finalScore = 0;
-var questionNumber = 0;
+int finalScore = 0;
+int questionNumber = 0;
+
+class QuestionAnswer {
+  String question;
+  List<String> options;
+  int correct;
+  QuestionAnswer({this.question, this.options, this.correct});
+}
 
 class _QuestionQuizState extends State<QuestionQuiz> {
-  var images = ['boy', 'girl', 'grandfather', 'grandmother'];
-  List questions = [
-    'Where is boy?',
-    'Where is girl?',
-    'Where is grandfather?',
-    'Where is grandmother?',
+  static List<String> items = [
+    'boy.png',
+    'girl.png',
+    'grandfather.png',
+    'grandmother.png'
+  ];
+  List<QuestionAnswer> questionList = [
+    QuestionAnswer(question: 'Đâu là bé trai?', options: items, correct: 1),
+    QuestionAnswer(question: 'Đâu là bé gái?', options: items, correct: 2),
+    QuestionAnswer(question: 'Đâu là ông?', options: items, correct: 3),
+    QuestionAnswer(question: 'Đâu là bà?', options: items, correct: 4)
   ];
 
-  int selected = 0;
+  void nextQuestion() {
+    if (selected == questionList[index].correct && index <= 3) {
+      setState(() {
+        finalScore++;
+        questionNumber++;
+      });
+      index++;
+    } else {
+      print('Bạn chưa trả lời câu hỏi');
+    }
+  }
 
+  int selected = 0;
+  int index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -83,7 +150,6 @@ class _QuestionQuizState extends State<QuestionQuiz> {
             Padding(
               padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
             ),
-            Text(questions[0]),
             Container(
               alignment: Alignment.centerRight,
               child: Row(
@@ -91,7 +157,18 @@ class _QuestionQuizState extends State<QuestionQuiz> {
                 children: <Widget>[
                   Text(
                     'Question ${questionNumber + 1}',
-                    style: TextStyle(fontSize: 20.0),
+                    style:
+                        TextStyle(fontSize: 20.0, fontStyle: FontStyle.italic),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(17, 0, 0, 0),
+                  ),
+                  Expanded(
+                    child: Text(
+                      questionList[index].question,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    ),
                   ),
                   Text(
                     'Score: ${finalScore}',
@@ -108,20 +185,12 @@ class _QuestionQuizState extends State<QuestionQuiz> {
                 RectItem(
                   imgURL: "assets/boy.png",
                   id: 1,
-
                   isActivate: selected == 1,
                   onClick: () {
-
                     setState(() {
                       selected = 1;
-
-
-
-
                     });
-
                   },
-
                 ),
                 Expanded(
                   child: Row(),
@@ -132,15 +201,14 @@ class _QuestionQuizState extends State<QuestionQuiz> {
                   isActivate: selected == 2,
                   onClick: () {
                     setState(() {
-                      selected = 2 ;
+                      selected = 2;
                     });
-
                   },
                 ),
               ],
             ),
             Padding(
-              padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
+              padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
             ),
             Row(
               children: <Widget>[
@@ -150,9 +218,8 @@ class _QuestionQuizState extends State<QuestionQuiz> {
                   isActivate: selected == 3,
                   onClick: () {
                     setState(() {
-                      selected = 3 ;
+                      selected = 3;
                     });
-
                   },
                 ),
                 Expanded(
@@ -164,9 +231,8 @@ class _QuestionQuizState extends State<QuestionQuiz> {
                   isActivate: selected == 4,
                   onClick: () {
                     setState(() {
-                      selected = 4 ;
+                      selected = 4;
                     });
-
                   },
                 ),
               ],
@@ -174,17 +240,23 @@ class _QuestionQuizState extends State<QuestionQuiz> {
             Padding(
               padding: EdgeInsets.fromLTRB(0, 15, 0, 15),
             ),
-            RaisedButton(
-
-              child: Text('Check'),
-              onPressed: () {
-
-                print("Giá trị selected là ${selected}");
-              },
-            )
-
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ButtonItem(isCorrect: selected == questionList[index].correct),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                ),
+                IconButton(
+                  color: Colors.green,
+                  icon: Icon(Icons.skip_next),
+                  onPressed: () {
+                    nextQuestion();
+                  },
+                )
+              ],
+            ),
           ],
-
         ),
       ),
     );
