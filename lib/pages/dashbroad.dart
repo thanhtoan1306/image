31cd9d42a4b7b9d13277b690d41a645dfa6d24b3
letoan <letoan0608@gmail.com>
2020-04-02 19:57:43 +0700

    update dashbroad

--- lib/pages/dashbroad.dart
+++ lib/pages/dashbroad.dart
@@ -1,5 +1,81 @@
 import 'package:flutter/material.dart';
 
+
+class WidgePlanet extends StatefulWidget {
+  String imgUrl;
+  WidgePlanet({this.imgUrl});
+  @override
+  _WidgePlanetState createState() => _WidgePlanetState();
+}
+
+class _WidgePlanetState extends State<WidgePlanet> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      width: 350,
+      child: Stack(
+        children: <Widget>[
+          Container(),
+
+          Positioned(
+              left: 50,
+              child:  Container(
+
+                height: 140,
+                width: 310,
+                decoration: BoxDecoration(
+                    color: Color(0xff363667),
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                child: Padding(padding: EdgeInsets.fromLTRB(70, 20, 0, 20)
+                  ,child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+
+                      Text('Earth',style: TextStyle(color: Color(0xfff8f8fe),fontSize: 20,fontWeight: FontWeight.bold),),
+                      Text('Minkyway Galaxy',style: TextStyle(color: Color(0xfff8f8fe),fontSize: 16),),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Row(
+                            children: <Widget>[
+                              Icon(Icons.location_on,color: Color(0xff504e7c),),
+                              Padding(padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                child: Text('3.711m/s',style: TextStyle(color: Color(0xffc0c0f5),fontSize: 16),),)
+                            ],
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Icon(Icons.vertical_align_bottom,color: Color(0xff504e7c),),
+                              Padding(padding: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                child: Text('3.711m/s',style: TextStyle(color: Color(0xffc0c0f5),fontSize: 16),),)
+                            ],
+                          )
+                        ],
+
+                      )
+                    ],
+                  ),),
+              )),
+          Positioned(
+              top: 20,
+
+              child: Container(
+                child: Image.asset(widget.imgUrl),
+                height: 100,
+                width: 100,
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+
 class DashBroad extends StatefulWidget {
   @override
   _DashBroadState createState() => _DashBroadState();
@@ -11,101 +87,28 @@ class _DashBroadState extends State<DashBroad> {
     return Scaffold(
         appBar: AppBar(title: Text('DashBroad')),
         body: Container(
-          color: Colors.white,
-          padding: EdgeInsets.fromLTRB(20, 20, 20, 30),
-          constraints: BoxConstraints.expand(),
-          child: Column(
+          color: Color(0xff6e6bb4),
+          child: ListView(
             children: <Widget>[
-              Text(
-                'Join a Norwegian Club!',
-                style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.grey,
-                    fontWeight: FontWeight.bold),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                child: Text(
-                  'Practice chatting in Norwegian with orther learners',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.grey[400],
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                child: Container(
-                  color: Colors.white,
-                  child: Image.asset('assets/group.png'),
-                  height: 300,
-                  width: double.infinity,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
-                child: Container(
-                    height: 50,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.lightBlueAccent),
-                    child: Center(
-                      child: Text(
-                        'FIND ME A CLUB',
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
-                      ),
-                    )),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                      height: 50,
-                      width: 170,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(30),
-                        border:
-                        Border.all(color: Colors.lightBlueAccent, width: 2.0),
-                      ),
-                      child: Center(
-                        child: Text(
-                          'CREATE',
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.lightBlueAccent,
-                              fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                      )),
-                  Container(
-                      height: 50,
-                      width: 170,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        borderRadius: BorderRadius.circular(30),
-                        border:
-                        Border.all(color: Colors.lightBlueAccent, width: 2.0),
-                      ),
-                      child: Center(
-                        child: Text(
-                          'CLUB CODE',
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.lightBlueAccent,
-                              fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                      )),
-                ],
-              )
+
+              Padding(padding: EdgeInsets.fromLTRB(10 ,30,10,0),
+              child:  WidgePlanet(
+                imgUrl: 'assets/starplune.png',
+              ), ),
+              Padding(padding: EdgeInsets.fromLTRB(10 ,30,10,0),
+                child:  WidgePlanet(
+                  imgUrl: 'assets/venus.png',
+                ), ),
+              Padding(padding: EdgeInsets.fromLTRB(10 ,30,10,0),
+                child:  WidgePlanet(
+                  imgUrl: 'assets/earth.png',
+                ), ),
+              Padding(padding: EdgeInsets.fromLTRB(10 ,30,10,0),
+                child:  WidgePlanet(
+                  imgUrl: 'assets/sunstart.png',
+                ), ),
+
+
 
             ],
           ),
